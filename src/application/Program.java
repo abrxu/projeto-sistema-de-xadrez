59546d62c4a4cad810beaa91a6daa96311944923
abrxu <abrxu@yahoo.com>
2024-07-02 16:59:48 -0300

    First class position

--- src/application/Program.java
+++ src/application/Program.java
@@ -5,7 +5,7 @@ import boardgame.Position;
 public class Program {
     public static void main(String[] args) {
 
-        Position pos = new Position(3, 5);
+        Position pos = new Position(4, 5);
         System.out.println(pos);
 
     }
